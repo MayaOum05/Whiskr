781a809b4da791845b1f599254d425f0c07e8162
Maya <maya.oum02@gmail.com>
2025-11-08 13:34:15 -0500

    added content to navbar

--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -4,6 +4,12 @@ export default function Navbar() {
     return (
         <nav className="navbar">
             <h1>Navigation</h1>
+            <ul>
+                <li>Dashboard</li>
+                <li>Pets</li>
+                <li>Appointmets</li>
+                <li>Medication</li>
+            </ul>
         </nav>
     )
 }
